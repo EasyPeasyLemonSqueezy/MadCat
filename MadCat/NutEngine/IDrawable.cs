b03223077a5c13fab845858a00b9e2f41313ee9b
Learko <matveevantonekb@gmail.com>
2016-10-30 17:38:08 +0500

    Add IDrawable interface from Test branch

---
+++ MadCat/NutEngine/IDrawable.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace NutEngine
+{
+    interface IDrawable
+    {
+        void Draw(SpriteBatch spriteBatch, Transform2D currentTransform);
+    }
+}
