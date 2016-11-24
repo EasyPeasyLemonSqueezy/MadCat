318cb0f6c5fabec364dcc9af9ce6add17ebb9cb6
Learko <matveevantonekb@gmail.com>
2016-11-24 21:34:28 +0500

    Add Length property

--- MadCat/NutPackerLib/SpriteSheet.cs
+++ MadCat/NutPackerLib/SpriteSheet.cs
@@ -3,11 +3,18 @@
     using Microsoft.Xna.Framework;
 
     /// <summary>
-    /// Contains array of <see cref="Rectangle"/> and indexed property getter for it.
+    /// Contains array of <see cref="Rectangle"/>,
+    /// Length property, and indexed property getter for it.
     /// </summary>
     public abstract class SpriteSheet
     {
         protected Rectangle[] Frames;
+
+        public int Length {
+            get {
+                return Frames.Length;
+            }
+        }
 
         public Rectangle this[int index] {
             get {
