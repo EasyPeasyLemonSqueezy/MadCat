6921085963b1294a36d6e43fbdcc8d5b8fb4cbd2
Learko <matveevantonekb@gmail.com>
2017-03-03 17:00:56 +0500

    Using srt creation method

--- MadCat/NutEngine/Camera/OrthographicSRTCamera.cs
+++ MadCat/NutEngine/Camera/OrthographicSRTCamera.cs
@@ -4,19 +4,14 @@ namespace NutEngine.Camera
 {
     public class OrthographicSRTCamera : Camera2D
     {
-        /// TODO: Считать матрицу без 4 умножений
-        /// и делать это по-разному в унаследованных камерах
         public override Matrix2D Transform {
             get {
                 return Matrix2D.CreateTranslation(-Origin)
-                     * Matrix2D.CreateTranslation(-Position)
-                     * Matrix2D.CreateRotation(Rotation)
-                     * Matrix2D.CreateScale(new Vector2(Zoom, Zoom))
+                     * Matrix2D.CreateSRT(-Position, new Vector2(Zoom, Zoom), Rotation)
                      * Matrix2D.CreateTranslation(Origin);
             }
         }
-
-
+        
         public OrthographicSRTCamera(Vector2 frame) : base(frame) { }
     }
 }
