1d53369dec45acbc08fa590b898b22740a7da920
Learko <matveevantonekb@gmail.com>
2017-03-11 02:27:13 +0500

    Update generated content

--- MadCat/MadCat/Demo.cs
+++ MadCat/MadCat/Demo.cs
@@ -11,12 +11,15 @@
 namespace NutPacker.Content
 {
     
+    [NutPackerLib.OriginalNameAttribute("Adventure girl")]
     public class AdventureGirl : NutPacker.ISpriteGroup
     {
+        [NutPackerLib.OriginalNameAttribute("Dead")]
         public class Dead : NutPacker.ISpriteSheet
         {
             private static Microsoft.Xna.Framework.Rectangle[] Frames = new Microsoft.Xna.Framework.Rectangle[] {
                     new Microsoft.Xna.Framework.Rectangle(5846, 0, 605, 604),
+                    new Microsoft.Xna.Framework.Rectangle(5128, 1084, 605, 604),
                     new Microsoft.Xna.Framework.Rectangle(5846, 604, 605, 604),
                     new Microsoft.Xna.Framework.Rectangle(3846, 2710, 605, 604),
                     new Microsoft.Xna.Framework.Rectangle(4487, 2168, 605, 604),
@@ -24,8 +27,7 @@ namespace NutPacker.Content
                     new Microsoft.Xna.Framework.Rectangle(5733, 1208, 605, 604),
                     new Microsoft.Xna.Framework.Rectangle(4451, 2772, 605, 604),
                     new Microsoft.Xna.Framework.Rectangle(5092, 2292, 605, 604),
-                    new Microsoft.Xna.Framework.Rectangle(5733, 1812, 605, 604),
-                    new Microsoft.Xna.Framework.Rectangle(5128, 1084, 605, 604)};
+                    new Microsoft.Xna.Framework.Rectangle(5733, 1812, 605, 604)};
             public int Length
             {
                 get
@@ -41,10 +43,12 @@ namespace NutPacker.Content
                 }
             }
         }
+        [NutPackerLib.OriginalNameAttribute("Idle")]
         public class Idle : NutPacker.ISpriteSheet
         {
             private static Microsoft.Xna.Framework.Rectangle[] Frames = new Microsoft.Xna.Framework.Rectangle[] {
                     new Microsoft.Xna.Framework.Rectangle(0, 1143, 641, 542),
+                    new Microsoft.Xna.Framework.Rectangle(641, 1143, 641, 542),
                     new Microsoft.Xna.Framework.Rectangle(1282, 1143, 641, 542),
                     new Microsoft.Xna.Framework.Rectangle(2000, 0, 641, 542),
                     new Microsoft.Xna.Framework.Rectangle(2000, 542, 641, 542),
@@ -52,8 +56,7 @@ namespace NutPacker.Content
                     new Microsoft.Xna.Framework.Rectangle(1923, 1143, 641, 542),
                     new Microsoft.Xna.Framework.Rectangle(2641, 542, 641, 542),
                     new Microsoft.Xna.Framework.Rectangle(3282, 0, 641, 542),
-                    new Microsoft.Xna.Framework.Rectangle(2564, 1084, 641, 542),
-                    new Microsoft.Xna.Framework.Rectangle(641, 1143, 641, 542)};
+                    new Microsoft.Xna.Framework.Rectangle(2564, 1084, 641, 542)};
             public int Length
             {
                 get
@@ -69,10 +72,12 @@ namespace NutPacker.Content
                 }
             }
         }
+        [NutPackerLib.OriginalNameAttribute("Jump")]
         public class Jump : NutPacker.ISpriteSheet
         {
             private static Microsoft.Xna.Framework.Rectangle[] Frames = new Microsoft.Xna.Framework.Rectangle[] {
                     new Microsoft.Xna.Framework.Rectangle(3282, 542, 641, 542),
+                    new Microsoft.Xna.Framework.Rectangle(3205, 1084, 641, 542),
                     new Microsoft.Xna.Framework.Rectangle(0, 1685, 641, 542),
                     new Microsoft.Xna.Framework.Rectangle(0, 2227, 641, 542),
                     new Microsoft.Xna.Framework.Rectangle(641, 1685, 641, 542),
@@ -80,8 +85,7 @@ namespace NutPacker.Content
                     new Microsoft.Xna.Framework.Rectangle(641, 2227, 641, 542),
                     new Microsoft.Xna.Framework.Rectangle(1282, 1685, 641, 542),
                     new Microsoft.Xna.Framework.Rectangle(641, 2769, 641, 542),
-                    new Microsoft.Xna.Framework.Rectangle(1282, 2227, 641, 542),
-                    new Microsoft.Xna.Framework.Rectangle(3205, 1084, 641, 542)};
+                    new Microsoft.Xna.Framework.Rectangle(1282, 2227, 641, 542)};
             public int Length
             {
                 get
@@ -97,6 +101,7 @@ namespace NutPacker.Content
                 }
             }
         }
+        [NutPackerLib.OriginalNameAttribute("Melee")]
         public class Melee : NutPacker.ISpriteSheet
         {
             private static Microsoft.Xna.Framework.Rectangle[] Frames = new Microsoft.Xna.Framework.Rectangle[] {
@@ -122,6 +127,7 @@ namespace NutPacker.Content
                 }
             }
         }
+        [NutPackerLib.OriginalNameAttribute("Run")]
         public class Run : NutPacker.ISpriteSheet
         {
             private static Microsoft.Xna.Framework.Rectangle[] Frames = new Microsoft.Xna.Framework.Rectangle[] {
@@ -148,6 +154,7 @@ namespace NutPacker.Content
                 }
             }
         }
+        [NutPackerLib.OriginalNameAttribute("Shoot")]
         public class Shoot : NutPacker.ISpriteSheet
         {
             private static Microsoft.Xna.Framework.Rectangle[] Frames = new Microsoft.Xna.Framework.Rectangle[] {
@@ -169,6 +176,7 @@ namespace NutPacker.Content
                 }
             }
         }
+        [NutPackerLib.OriginalNameAttribute("Slide")]
         public class Slide : NutPacker.ISpriteSheet
         {
             private static Microsoft.Xna.Framework.Rectangle[] Frames = new Microsoft.Xna.Framework.Rectangle[] {
@@ -193,10 +201,13 @@ namespace NutPacker.Content
             }
         }
     }
+    [NutPackerLib.OriginalNameAttribute("Graveyard")]
     public class Graveyard : NutPacker.ITileSet
     {
+        [NutPackerLib.OriginalNameAttribute("Objects")]
         public class Objects : NutPacker.ITileSet
         {
+            [NutPackerLib.OriginalNameAttribute("ArrowSign.png")]
             public static Microsoft.Xna.Framework.Rectangle ArrowSign
             {
                 get
@@ -204,6 +215,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(681, 3695, 86, 87);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Bush (1).png")]
             public static Microsoft.Xna.Framework.Rectangle Bush_1_
             {
                 get
@@ -211,6 +223,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(0, 3550, 182, 90);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Bush (2).png")]
             public static Microsoft.Xna.Framework.Rectangle Bush_2_
             {
                 get
@@ -218,6 +231,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(490, 3695, 100, 64);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Crate.png")]
             public static Microsoft.Xna.Framework.Rectangle Crate
             {
                 get
@@ -225,6 +239,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(384, 3695, 106, 106);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("DeadBush.png")]
             public static Microsoft.Xna.Framework.Rectangle DeadBush
             {
                 get
@@ -232,6 +247,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(286, 3311, 132, 74);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Sign.png")]
             public static Microsoft.Xna.Framework.Rectangle Sign
             {
                 get
@@ -239,6 +255,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(590, 3695, 91, 93);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Skeleton.png")]
             public static Microsoft.Xna.Framework.Rectangle Skeleton
             {
                 get
@@ -246,6 +263,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(2000, 1084, 102, 50);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("TombStone (1).png")]
             public static Microsoft.Xna.Framework.Rectangle TombStone_1_
             {
                 get
@@ -253,6 +271,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(2102, 1084, 54, 55);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("TombStone (2).png")]
             public static Microsoft.Xna.Framework.Rectangle TombStone_2_
             {
                 get
@@ -260,6 +279,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(310, 3513, 53, 76);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tree.png")]
             public static Microsoft.Xna.Framework.Rectangle Tree
             {
                 get
@@ -268,8 +288,10 @@ namespace NutPacker.Content
                 }
             }
         }
+        [NutPackerLib.OriginalNameAttribute("Tiles")]
         public class Tiles : NutPacker.ITileSet
         {
+            [NutPackerLib.OriginalNameAttribute("BG.png")]
             public static Microsoft.Xna.Framework.Rectangle BG
             {
                 get
@@ -277,6 +299,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(0, 0, 2000, 1143);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Bones (1).png")]
             public static Microsoft.Xna.Framework.Rectangle Bones_1_
             {
                 get
@@ -284,6 +307,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(0, 3640, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Bones (2).png")]
             public static Microsoft.Xna.Framework.Rectangle Bones_2_
             {
                 get
@@ -291,6 +315,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(286, 3385, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Bones (3).png")]
             public static Microsoft.Xna.Framework.Rectangle Bones_3_
             {
                 get
@@ -298,6 +323,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(418, 3311, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Bones (4).png")]
             public static Microsoft.Xna.Framework.Rectangle Bones_4_
             {
                 get
@@ -305,6 +331,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(182, 3550, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (1).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_1_
             {
                 get
@@ -312,6 +339,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(414, 3439, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (10).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_10_
             {
                 get
@@ -319,6 +347,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(546, 3311, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (11).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_11_
             {
                 get
@@ -326,6 +355,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(128, 3678, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (12).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_12_
             {
                 get
@@ -333,6 +363,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(256, 3678, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (13).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_13_
             {
                 get
@@ -340,6 +371,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(384, 3567, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (14).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_14_
             {
                 get
@@ -347,6 +379,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(542, 3439, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (15).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_15_
             {
                 get
@@ -354,6 +387,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(674, 3311, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (16).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_16_
             {
                 get
@@ -361,6 +395,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(512, 3567, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (2).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_2_
             {
                 get
@@ -368,6 +403,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(670, 3439, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (3).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_3_
             {
                 get
@@ -375,6 +411,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(802, 3311, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (4).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_4_
             {
                 get
@@ -382,6 +419,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(640, 3567, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (5).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_5_
             {
                 get
@@ -389,6 +427,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(798, 3439, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (6).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_6_
             {
                 get
@@ -396,6 +435,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(930, 3311, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (7).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_7_
             {
                 get
@@ -403,6 +443,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(768, 3567, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (8).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_8_
             {
                 get
@@ -410,6 +451,7 @@ namespace NutPacker.Content
                     return new Microsoft.Xna.Framework.Rectangle(926, 3439, 128, 128);
                 }
             }
+            [NutPackerLib.OriginalNameAttribute("Tile (9).png")]
             public static Microsoft.Xna.Framework.Rectangle Tile_9_
             {
                 get
