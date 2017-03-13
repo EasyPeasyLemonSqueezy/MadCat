67908672ab8f25a7c92f1e2240616357bcecd7bf
Denis Vyatkin <catinjar42@gmail.com>
2017-03-13 13:35:50 +0500

    CollisionDetector constructor

--- MadCat/NutEngine/CollisionDetector.cs
+++ MadCat/NutEngine/CollisionDetector.cs
@@ -5,19 +5,23 @@ namespace NutEngine
 {
     public class CollisionDetector
     {
-        public delegate void Callback();
+        public delegate void Callback(AABB first, AABB second);
 
         private Dictionary<Tuple<Type, Type>, Callback> typeRules;
+
+        public CollisionDetector()
+        {
+            typeRules = new Dictionary<Tuple<Type, Type>, Callback>();
+        }
 
         public void CheckCollisions(IEnumerable<GameObject> entities)
         {
             foreach (var first in entities) {
                 foreach (var second in entities) {
                     var types = new Tuple<Type, Type>(first.GetType(), second.GetType());
-
                     if (typeRules.ContainsKey(types)) {
                         if (first.Collider.Intersects(second.Collider)) {
-                            typeRules[types]();
+                            typeRules[types](first.Collider, second.Collider);
                         }
                     }
                 }
