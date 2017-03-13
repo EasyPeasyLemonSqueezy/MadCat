298441d229f66e03da8fb3fe3ed21526c75b6d49
Denis Vyatkin <catinjar42@gmail.com>
2017-03-13 12:28:42 +0500

    Add CollisionDetector

---
+++ MadCat/NutEngine/GameObject.cs
@@ -0,0 +1,14 @@
+﻿namespace NutEngine
+{
+    public abstract class GameObject
+    {
+        public AABB Collider { get; set; }
+
+        public abstract void Update(float deltaTime);
+
+        public bool Collide(GameObject other)
+        {
+            return Collide((dynamic) other);
+        }
+    }
+}
