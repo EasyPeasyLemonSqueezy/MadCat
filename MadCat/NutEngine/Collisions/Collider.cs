e65302d11fc64bee0dc76b26e68b608afab12a51
Denis Vyatkin <catinjar42@gmail.com>
2017-03-13 15:36:29 +0500

    Add comments

--- MadCat/NutEngine/Collisions/Collider.cs
+++ MadCat/NutEngine/Collisions/Collider.cs
@@ -1,5 +1,8 @@
 ﻿namespace NutEngine
 {
+    /// <summary>
+    /// Base class for all shapes that can collide
+    /// </summary>
     public abstract class Collider
     {
         public bool Intersects(Collider other)
