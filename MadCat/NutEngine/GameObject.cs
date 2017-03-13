1c07220f220b4026bb7ba4b35f34e20875bc0fd1
Denis Vyatkin <catinjar42@gmail.com>
2017-03-13 13:25:38 +0500

    Collision detection for types

--- MadCat/NutEngine/GameObject.cs
+++ MadCat/NutEngine/GameObject.cs
@@ -3,12 +3,6 @@
     public abstract class GameObject
     {
         public AABB Collider { get; set; }
-
         public abstract void Update(float deltaTime);
-
-        public bool Collide(GameObject other)
-        {
-            return Collide((dynamic) other);
-        }
     }
 }
