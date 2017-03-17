aadd022b32632fd9570a6d43b5b1d32406f91524
Learko <matveevantonekb@gmail.com>
2017-03-18 02:35:52 +0500

    Clear solution

--- MadCat/NutEngine/IDrawable.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Xna.Framework.Graphics;
-
-namespace NutEngine
-{
-    public interface IDrawable
-    {
-        void Draw(SpriteBatch spriteBatch, Matrix2D currentTransform);
-    }
-}
