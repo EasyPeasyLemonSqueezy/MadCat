3e485894f3f77d2b4bcfec1799b01c47a34910c1
Denis Vyatkin <catinjar42@gmail.com>
2017-03-25 00:29:43 +0500

    UpdateInput

--- MadCat/NutEngine/StateMachine.cs
+++ MadCat/NutEngine/StateMachine.cs
@@ -9,12 +9,21 @@
             currentState = firstState;
         }
 
+        public void UpdateInput(Input.KeyboardState keyboardState)
+        {
+            var newState = currentState.UpdateInput(keyboardState);
+
+            if (newState != null) {
+                currentState = newState;
+                currentState.Enter();
+            }
+        }
+
         public void Update(float deltaTime)
         {
             var newState = currentState.Update(deltaTime);
 
             if (newState != null) {
-                currentState.Exit();
                 currentState = newState;
                 currentState.Enter();
             }
