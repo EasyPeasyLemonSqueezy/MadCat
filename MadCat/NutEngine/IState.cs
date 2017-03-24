e695a808781e5114ac7d222a7befb91ac28065e0
Denis Vyatkin <catinjar42@gmail.com>
2017-03-24 23:27:02 +0500

    States and StateMachines

---
+++ MadCat/NutEngine/IState.cs
@@ -0,0 +1,10 @@
+﻿namespace NutEngine
+{
+    public interface IState
+    {
+        void Enter();
+        void Exit();
+        IState UpdateInput(Input.KeyboardState keyboardState);
+        IState Update(float deltaTime);
+    }
+}
---
+++ MadCat/NutEngine/StateMachine.cs
@@ -0,0 +1,23 @@
+﻿namespace NutEngine
+{
+    public class StateMachine
+    {
+        private IState currentState;
+
+        public StateMachine(IState firstState)
+        {
+            currentState = firstState;
+        }
+
+        public void Update(float deltaTime)
+        {
+            var newState = currentState.Update(deltaTime);
+
+            if (newState != null) {
+                currentState.Exit();
+                currentState = newState;
+                currentState.Enter();
+            }
+        }
+    }
+}
