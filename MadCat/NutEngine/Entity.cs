2a4978f2559c856b2b5e6ae006c1cfc1e30cdc5e
Denis Vyatkin <catinjar42@gmail.com>
2017-04-05 15:17:16 +0500

    Entities and components

---
+++ MadCat/NutEngine/Entity.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NutEngine
+{
+    public class Entity : ICleanup
+    {
+        public bool Invalid { get; set; }
+        public AABB Collider { get; set; }
+
+        private Dictionary<Type, Component> components =
+            new Dictionary<Type, Component>();
+
+        public void Update(float deltaTime)
+        {
+            foreach (var component in components.Values) {
+                component.Update(deltaTime);
+            }
+        }
+
+        public void Cleanup()
+        {
+            foreach (var component in components.Values) {
+                if (component is ICleanup cleanup) {
+                    cleanup.Cleanup();
+                }
+            }
+        }
+
+        public Entity AddComponent(Component component)
+        {
+            components[component.GetType()] = component;
+            return this;
+        }
+
+        public Entity RemoveComponent<T>()
+        {
+            components.Remove(typeof(T));
+            return this;
+        }
+
+        public bool HasComponent<T>()
+        {
+            return components.ContainsKey(typeof(T));
+        }
+
+        public Component GetComponent<T>()
+        {
+            if (HasComponent<T>()) {
+                return components[typeof(T)];
+            }
+            return null;
+        }
+    }
+}
