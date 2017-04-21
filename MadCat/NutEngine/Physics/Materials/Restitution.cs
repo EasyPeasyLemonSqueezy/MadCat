d2f5e667d4a44a40375380f8bd9d1c73bd2bf13a
Learko <matveevantonekb@gmail.com>
2017-04-21 16:30:20 +0500

    Add default materials

---
+++ MadCat/NutEngine/Physics/Materials/Restitution.cs
@@ -0,0 +1,13 @@
+﻿namespace NutEngine.Physics.Materials
+{
+    public static class Restitution
+    {
+        public const float Rock = .1f;
+        public const float Wood = .2f;
+        public const float Metal = .05f;
+        public const float BouncyBall = .8f;
+        public const float SuperBall = .95f;
+        public const float Pillow = .2f;
+        public const float Static = 4f;
+    }
+}
